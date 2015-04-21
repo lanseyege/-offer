e2fcba9947a277575db86c414caa5d56f5e8dbb9
lanseyege <yuan1989yeye@163.com>
2015-04-21 21:49:12 +0800

    Create 4_replace_blank.c

---
+++ 4_replace_blank.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void replace(char string[], int length){
+  if(string == null || length<=0)
+    return;
+  int n, i=0;
+  for(i=0;i<length;i++){
+    if(string[i]== ' ')
+      n++;
+  }
+  int p1=length;
+  length += n * 3;
+  int p2=length;
+  while(p1 >0){
+  while(string[p1] != ' ' ){
+    string[p2]=string[p1];
+    p2--;
+    p1--;
+  }
+  string[p2--]='0';
+  string]p2--]='2';
+  string[p2--]='%';
+  p1--;
+  }
+}
