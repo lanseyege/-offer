200066157cb776e4edd410bd1767cd1566a0f498
lanseyege <yuan1989yeye@163.com>
2015-04-26 12:21:12 +0800

    Create 25_print_btree_number.c

---
+++ 25_print_btree_number.c
@@ -0,0 +1,20 @@
+
+bool btree_number(binary_tree_node* root, int number){
+  if(root == NULL)
+    return false;
+  if(root->value > number)
+    return false;
+  if(root->left == NULL && root->right == NULL)
+    if(root->value == number)
+      print();
+      return true;
+    else
+      return false;
+  else
+    bool left = false, right = false;
+    if((left = btree_number(root->left, number - root->value)))
+      print()
+    if(right = btree_number(root->right,number - root->value)))
+      print();
+    return left || right;
+}
