596ebce8e9c0c2076749dc00cd398908c82ec91e
lanseyege <yuan1989yeye@163.com>
2015-04-26 08:27:59 +0800

    Update 21_stack_min.c

--- 21_stack_min.c
+++ 21_stack_min.c
@@ -1,1 +1,22 @@
-
+push(int data){
+  if(m_stack.empty()){
+    m_stack.push_back(data);
+    m_min.push_back(data);
+  }else{
+    m_stack.push_back(data);
+    int temp = m_min.top();
+    if(temp>data)
+      m_min.push_back(data);
+    else
+      m_min.push_back(temp);
+  }
+}
+pop(){
+  assert(m_stack.size()>0 && m_min.size() >0 );
+  m_stack.pop();
+  m_min.pop();
+}
+min(){
+  assert(m_stack.size()>0 && m_min.size()>0);
+  m_min.top();
+}
