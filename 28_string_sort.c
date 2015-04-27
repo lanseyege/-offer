38a9461887b10969e3ea71549e2892c4178a2ff9
lanseyege <yuan1989yeye@163.com>
2015-04-27 09:35:19 +0800

    Create 28_string_sort.c

---
+++ 28_string_sort.c
@@ -0,0 +1,13 @@
+void orint_string(char* string){
+  if(string == NULL)
+    return NULL;
+  int n = strlen(string);
+  print_string_it(string, n, 0);
+}
+void print_string_it(char* string, int n, int index){
+  if(index == n-1)
+    print
+  for(int i=0; i<n;i++){
+    pirnt_string_it(string[i]
+  }
+}
