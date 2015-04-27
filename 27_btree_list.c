6bb709be0514e2f9bc0491413923b963ee7b1c38
lanseyege <yuan1989yeye@163.com>
2015-04-27 09:06:52 +0800

    Update 27_btree_list.c

--- 27_btree_list.c
+++ 27_btree_list.c
@@ -1,12 +1,18 @@
-void tree_double_list(binary_tree_node* root, binary_tree_node* node){
-  if(root == NULL)
-    return NULL;
-  if(root->left != NULL)
-    tree_double_list(root->left, node);
-  root->left = node;
-  node=root;
-  if(root->right != NULL)
-    tree_double_list(root->right,node);
-  root->right = node;
+void tree_double_list(binary_tree_node* current_node, binary_tree_node* last_node){
   
+  if(current_node == NULL)
+    return NULL;
+    
+  if(current_node->left != NULL)
+    tree_double_list(current_node->left, last_node);
+    
+  current_node->left = last_node;
+  
+  if(last_node != NULL)
+    last_node->right = current;
+    
+  last_node = current_node;
+  
+  if(current_node->right != NULL)
+    tree_node_list(current_node->right, last_node);
 }
