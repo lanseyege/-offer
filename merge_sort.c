1145c0f30653e9e3ea5d8c137eaa2216fa5e023b
lanseyege <yuan1989yeye@163.com>
2015-05-05 15:05:50 +0800

    Update merge_sort.c

--- merge_sort.c
+++ merge_sort.c
@@ -12,6 +12,9 @@ void merge(int* A,int p, int q, int r){
     L[i]=A[p+i];
   for(j=0;j<n2;j++)
     R[j]=A[q+j+1];
+  L[n1] = INT_MAX;
+  R[n2] = INT_MAX;
+  i=0,j=0;
   for(k=p;k<=r;k++){
     if(L[i]<=R[j]){
       A[k]=L[i++];
