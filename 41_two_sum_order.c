47f1a8256a312f43ca5c780825a14cd9277c1ed2
lanseyege <yuan1989yeye@163.com>
2015-05-06 14:43:04 +0800

    Update 41_two_sum_order.c

--- 41_two_sum_order.c
+++ 41_two_sum_order.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int two_sum(int* A, int length, int k, int* num1, int* num2){
+  if(A==NULL||length<=0)
+    return 0;
+  int low=0,high=length-1;
+  while(low<high){
+    long long sum=A[low]+A[high];
+    if(sum==k){
+      *num1=low;
+      *num2=high;
+      return;
+    }else if(sum>k){
+      high--;
+    }else if(sum<k){
+      low++;
+    }
+  }
+  return 0;
+}
+int main(){
+  int A[]={1.2.4.7.11.15};
+  int num1.num2;
+  if(two_sum(A,6,15,&num1,&num2))
+    printf("%d %d\n",num1,num2);
+  else
+    printf("%s\n","not found~~");
+}
